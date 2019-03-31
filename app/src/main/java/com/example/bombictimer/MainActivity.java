97cce710ca5b27b4d8261332d3fd0e0277abb2f3
Mortemis <artyom.paskin@gmail.com>
2019-03-31 04:09:39 +0400

    Initial commit

---
+++ app/src/main/java/com/example/bombictimer/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.bombictimer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer = new Timer();
+    private final int secondsStep = 10;
+    private int minutes = 0;
+    private int seconds = 0;
+    private boolean isActive = false;
+
+    String time = "00:00";
+
+    @SuppressLint("HandlerLeak") //Static handler doesn't work here because of vibrator.
+    Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            if(msg.what == 0){
+                Toast.makeText(MainActivity.this.getApplicationContext(), "Boom!", Toast.LENGTH_SHORT).show();
+                long[] pattern = { 0, 300, 200, 300, 200, 300};
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vibrator.hasVibrator()) {
+                    vibrator.vibrate(pattern, -1);
+                }
+            } else if (msg.what == 1) {
+                TextView timerView = findViewById(R.id.timerView);
+                timerView.setText(time);
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startTimer(View view) {
+        if (!isActive) {
+            if(minutes != 0 || seconds != 0) {
+                timer.schedule(new TimerTask() {
+                    int estimatedTime = minutes * 60 + seconds;
+
+                    @Override
+                    public void run() {
+                        estimatedTime--;
+                        seconds = estimatedTime % 60;
+                        minutes = estimatedTime / 60;
+                        updateTimerView();
+                        if (estimatedTime == 0) {
+                            onTimerAlarm();
+                            this.cancel();
+                        }
+                    }
+                }, 0, 1000);
+                isActive = true;
+            }
+        }
+    }
+
+    private void onTimerAlarm() {
+
+        handler.sendEmptyMessage(0);
+        isActive = false;
+    }
+
+    public void updateTimerView() {
+        String min;
+        if (minutes/10 == 0) {
+            min = "0"+minutes;
+        } else {
+            min = ""+minutes;
+        }
+        String sec;
+        if (seconds/10 == 0) {
+            sec = "0"+seconds;
+        } else {
+            sec = ""+seconds;
+        }
+        time = min + ":" + sec;
+        handler.sendEmptyMessage(1);
+    }
+
+
+    public void incSeconds(View view) {
+        if (!isActive) {
+            seconds += secondsStep;
+            if (seconds >= 60) {
+                seconds = 0;
+                if (minutes < 60) {
+                    minutes++;
+                }
+            }
+            updateTimerView();
+        }
+    }
+
+    public void decSeconds(View view) {
+        if (!isActive) {
+            seconds -= secondsStep;
+            if (seconds < 0) {
+                seconds = 60-secondsStep;
+                if (minutes > 0) {
+                    minutes--;
+                } else {
+                    seconds = 0;
+                }
+            }
+            updateTimerView();
+        }
+    }
+
+    public void incMinutes(View view) {
+        if (!isActive) {
+            if (minutes <= 60) {
+                minutes++;
+            }
+            updateTimerView();
+        }
+    }
+
+    public void decMinutes(View view) {
+        if (!isActive) {
+            if (minutes > 0) {
+                minutes--;
+            }
+            updateTimerView();
+        }
+    }
+}
